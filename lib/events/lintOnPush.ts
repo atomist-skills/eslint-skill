1a2116a38f2f12faa5d3c659ee9f94af452b0906
Christian Dupuis <cd@atomist.com>
2020-06-18 18:50:39 +0200

    Fix pr search

--- lib/events/lintOnPush.ts
+++ lib/events/lintOnPush.ts
@@ -441,7 +441,7 @@ ${formatMarkers(ctx)}
                         repo: repo.name,
                         state: "open",
                         base: push.branch,
-                        head: `${repo.owner}:${branch}`,
+                        head: branch,
                         per_page: 100,
                     })
                 ).data;
